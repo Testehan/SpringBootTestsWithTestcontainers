b4c1e96bc3fe86f431d48390ac435f62b57f3684
DanTestehan <tdan89@yahoo.com>
2023-10-10 08:50:32 +0300

    added code that will be tested

---
+++ src/main/java/com/testehan/SpringBootTestsWithTestcontainers/post/PostRepository.java
@@ -0,0 +1,10 @@
+package com.testehan.SpringBootTestsWithTestcontainers.post;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.Optional;
+
+public interface PostRepository extends ListCrudRepository<Post,Integer> {
+
+    Optional<Post> findByTitle(String title);
+}
---
+++ src/main/java/com/testehan/SpringBootTestsWithTestcontainers/post/Posts.java
@@ -0,0 +1,6 @@
+package com.testehan.SpringBootTestsWithTestcontainers.post;
+
+import java.util.List;
+
+public record Posts(List<Post> posts) {
+}
